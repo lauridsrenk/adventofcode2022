099eff5ef1ce33b8e0305376b8f417814f1b772f
Laurids Renk <renklaurids@gmail.com>
2022-12-06 19:52:13 +0100

    simplified day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -8,33 +8,26 @@ fn main() {
     let file = args.next().unwrap();
     let content = fs::read_to_string(file).unwrap();
     let pairs = content.trim().lines().map(|line| {
-        let tasks = line.split_once(",").unwrap();
-        [tasks.0, tasks.1].map(|task| {
-            task.split("-")
-                .flat_map(i32::from_str)
-                .next_chunk::<2>()
-                .unwrap()
-        })
+        line.split([',', '-'])
+            .flat_map(i32::from_str)
+            .next_chunk::<4>()
+            .unwrap()
     });
 
-    if part == "part1" {
-        let overlap = pairs
-            .filter(|pair| {
-                (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1])
-                    || (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1])
-            })
-            .inspect(|pair| println!("{pair:?}"));
-        let answ = overlap.count();
-        println!("answer to part 1: {answ}");
+    let filter = if part == "part1" {
+        |pair: &[i32; 4]| {
+            (pair[0] <= pair[2] && pair[1] >= pair[3]) || (pair[0] >= pair[2] && pair[1] <= pair[3])
+        }
     } else if part == "part2" {
-        let overlap = pairs
-            .filter(|pair| {
-                (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][0])
-                    || (pair[0][0] <= pair[1][1] && pair[0][1] >= pair[1][1])
-                    || (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1])
-            })
-            .inspect(|pair| println!("{pair:?}"));
-        let answ = overlap.count();
-        println!("answer to part 2: {answ}");
-    }
+        |pair: &[i32; 4]| {
+            (pair[0] <= pair[2] && pair[1] >= pair[2])
+                || (pair[0] <= pair[3] && pair[1] >= pair[3])
+                || (pair[0] >= pair[2] && pair[1] <= pair[3])
+        }
+    } else {
+        panic!("part is neither part1 nor part2")
+    };
+
+    let count = pairs.filter(filter).count();
+    println!("{count}");
 }
